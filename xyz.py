960ef88dfc8ba96c2ac3c758cd0b74a27bd2c4b1
Mahmoud Hamed <dev.mahmoudhamed@gmail.com>
2021-08-15 10:13:25 +0200

    make b2 contain only java files

--- xyz.py
+++
@@ -1,6 +0,0 @@
- weekdays = ["Sunday", "Monday", "Tuesday","Wednesday", "Thursday","Friday", "Saturday"]
-print("Seven Weekdays are:\n")
-# Iterate the list using for loop
-
-for day in range(len(weekdays)):
-    print(weekdays[day])
